f1511baf29ec8f1d75d1c30f989737c3d4680a7f
alaabadra <alaabadra4@gmail.com>
2019-06-10 23:07:57 +0300

    create offer apps

--- server/database/queries/applications/getOfferApps.js
+++ server/database/queries/applications/getOfferApps.js
@@ -1,16 +1,16 @@
 const connection = require('../../config/db_connection');
 
 const getOfferApps = (offerId) => {
-  const selectOfferApps = {
-    text: `select
-    application.member_id, member.username , member.full_name , member.avatar, application.proposal, hired_member.status
-    from application
-    left outer join member on application.member_id= member.id
-    left outer join hired_member on (application.member_id=hired_member.member_id and application.offer_id=hired_member.offer_id)
-    where (application.offer_id=$1)`,
-    values: [offerId],
-  };
-  return connection.query(selectOfferApps);
+const selectOfferApps = {
+text: `select
+application.member_id, member.username , member.full_name , member.avatar, application.proposal, hired_member.status
+from application
+left outer join member on application.member_id= member.id
+left outer join hired_member on (application.member_id=hired_member.member_id and application.offer_id=hired_member.offer_id)
+where (application.offer_id=$1)`,
+values: [offerId],
+};
+return connection.query(selectOfferApps);
 };
 
-module.exports = getOfferApps;
+module.exports = getOfferApps; 
